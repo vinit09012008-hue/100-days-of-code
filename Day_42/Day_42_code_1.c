513482b58ca958af697e6334316fa7e03513f659
vinit09012008-hue <vinit09012008@gmail.com>
2026-09-20 20:43:24 +0530

    Add files via upload

---
+++ Day_42/Day_42_code_1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+Q83: Count vowels and consonants in a string.
+int main() {
+
+    char str[100];
+    int i = 0;
+    int vowels = 0, consonants = 0;
+
+    scanf("%s", str);
+
+    while(str[i] != '\0') {
+
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||
+           str[i]=='o' || str[i]=='u' ||
+           str[i]=='A' || str[i]=='E' || str[i]=='I' ||
+           str[i]=='O' || str[i]=='U') {
+
+            vowels++;
+        }
+
+        else if((str[i]>='a' && str[i]<='z') ||
+                (str[i]>='A' && str[i]<='Z')) {
+
+            consonants++;
+        }
+
+        i++;
+    }
+
+    printf("Vowels=%d, Consonants=%d", vowels, consonants);
+
+    return 0;
+}
---
+++ Day_42/Day_42_code_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//Q84: Convert a lowercase string to uppercase without using built-in functions.
+int main() {
+
+    char str[100];
+    int i = 0;
+
+    scanf("%s", str);
+
+    while(str[i] != '\0') {
+
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+
+        i++;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
